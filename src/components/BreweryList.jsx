5c003aaef3be4bc3c7a5529be71a5e9b43adcf70
Tenzin Jamyang <jamyangu@gmail.com>
2021-08-21 23:47:58 -0400

    api requirements are met and on to styling

--- src/components/BreweryList.jsx
+++ src/components/BreweryList.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 class BreweryList extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {  };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
+     };
+     this.fetchData = this.fetchData.bind(this)
+  }
+
+  componentDidMount(){
+    this.fetchData()
+  }
+
+  fetchData () {
+    let latitude
+    let longitude
+    navigator.geolocation.getCurrentPosition(async (position) => {
+          latitude = position.coords.latitude
+          longitude = position.coords.longitude
+          console.log(latitude, longitude)
+          let data = await fetch(`https://api.openbrewerydb.org/breweries?by_dist=${latitude},${longitude}&per_page=50`)
+          let breweries = await data.json()
+          console.log(breweries)
+          this.setState( {list: breweries} )
+        })
+    console.log(this.state.latitude)
+  }
+
+  compileAddress(item){
+    return `${item.street}, ${item.city}, ${item.state}, ${item.postal_code}`
+  }
 
   render() {
     return (
@@ -12,8 +38,10 @@ class BreweryList extends Component {
         <h1>Welcome to Elixir</h1>
         <img src="https://heltonbrewing.com/wp-content/uploads/2015/07/Brewery.jpg" alt="stockImage" />
         <button>Breweries Near You!!</button>
+        <ol>{this.state.list.map(brewery => <li key={brewery.id}>
+          <a href={brewery.website_url}>{brewery.name}</a>, {brewery.brewery_type},{this.compileAddress(brewery)}</li>)}
+        </ol>
       </div>
-
     );
   }
 }
