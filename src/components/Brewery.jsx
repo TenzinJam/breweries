4858036e99b458ef0fa8ae54eb6eb4bc86950efa
Tenzin Jamyang <jamyangu@gmail.com>
2021-08-24 00:20:05 -0400

    map component is almost working. default props is a little messy

--- src/components/Brewery.jsx
+++ src/components/Brewery.jsx
@@ -1,26 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import ReactLoading from 'react-loading'
+import GoogleMapReact from 'google-map-react'
+import key from '../config/key'
 
-function Brewery(){
+Brewery.defaultProps = {
+  zoom: 11
+}
+
+function Brewery(props){
   const [brewery, setBrewery] = useState(null)
   const { id } = useParams()
+
 
   useEffect(() => {
     async function fetchData(){
       let res = await fetch(`https://api.openbrewerydb.org/breweries/${id}`)
       res = await res.json()
       setBrewery(res)
+
     }
     fetchData()
-  }, [])
+  }, [id])
 
   return (
-    <div>
-      {brewery ? <h1>{brewery.name}</h1>: <ReactLoading type="spinningBubbles" color="" height={70} width={300}/>}
+    <div style={{ height: '50vh', width: '50%', marginTop: '80px', marginLeft: '40px'}}>
+      <div>{brewery ?
+        (<div>
+          <h1>{brewery.name}</h1>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: key.apiKey }}
+            defaultCenter={{lat: brewery.latitude, lng: brewery.longitude}}
+            defaultZoom={props.zoom}
+          >
+          </GoogleMapReact>
+        </div>)
+        : <ReactLoading type="spinningBubbles" color="" height={70} width={300}/>}</div>
+
     </div>
     )
-
 }
 
 export default Brewery
---
+++ src/config/key.js
@@ -0,0 +1,4 @@
+
+export default {
+  apiKey: "AIzaSyAEOkJUIzMA0snVbjf4L0MCHi06OgeHxCA"
+}
